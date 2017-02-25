5974693094c127cafdc5ffe564231c99c085f640
Craig <tingeycraig@gmail.com>
2017-02-25 10:47:48 -0700

    working on players

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,14 +2,45 @@ import React, { Component } from 'react';
 import '../styles/Player.css';
 
 class Player extends Component {
+  constructor(){
+    super()
+    this.state = {
+      voteStatus: "reject",
+      isCommander: false,
+      isAgent: true
+    }
+  }
+  renderReject() {
+    return (
+      <div className="rejectCard">
+        <span>Reject</span>
+      </div>
+    )
+  }
+  renderApprove() {
+    return (
+      <div className="approveCard">
+        Approve
+      </div>
+    )
+  }
+  renderVote() {
+    return (
+      <div className="vote-card">
+        { this.state.voteStatus == 'reject' ?
+          this.renderReject() :
+          this.renderApprove() }
+      </div>
+    )
+  }
   render() {
     return (
       <div className="full-height">
         <div className="player-actions">
-
+          { this.props.showVotes ? this.renderVote() : null}
         </div>
         <div className="player-name">
-          player {this.props.p_num}
+          {this.props.name} {this.props.p_num}
         </div>
       </div>
     );
--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -8,12 +8,12 @@ class Players extends Component {
     super();
   }
   render() {
-    var players = [];
-    var styleClass = 'col-'.concat(this.props.numPlayers);
+    let players = [];
+    let styleClass = 'col-'.concat(this.props.numPlayers);
     for (var i = 0; i < parseInt(this.props.numPlayers); i++) {
       players.push(
         <div key={i} className={'player-col ' + styleClass}>
-          <Player p_num={i+1} />
+          <Player p_num={i+1} name={"player"} showVotes={true} />
         </div>
       )
     }
